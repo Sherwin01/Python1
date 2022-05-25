ca2e609cf4fc939dc4e2ef91b695237f7acef3a5
lim <sherwinlim8899@gmail.com>
2022-05-25 21:56:53 +0800

    8th commit

---
+++ function.py
@@ -0,0 +1,14 @@
+def greeting():
+    print("Hello!")
+
+def your_name(name):
+    print(name)
+
+def your_last_name(lastname):
+    print(lastname)
+
+def your_age(age):
+    print(age)
+
+def your_gender(gender):
+    print(gender)
--- main.py
+++ main.py
@@ -1,26 +1,6 @@
-x = 500
-if x > 100:
-    raise Exception("if x is bigger than 100 this will be an error")
+from function import your_name
+from function import your_gender
 
-try:
-    print(variable_1)
-
-except:
-    print("variable_1 is not defined")
-
-for i in range(6):
-    print("i'm so happy!")
-
-try:
-    print(12*6)
-except:
-    print("this operation cant be performed.")
-else:
-    print("this can be performed")
-
-best_burger = "Burger King"
-number_2_burger = "McDonald's"
-
-assert best_burger == "Burger King"
-assert best_burger == "McDonald's"
+your_name("Lim")
+your_gender("Male")
 
