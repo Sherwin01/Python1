5f2153279cb44826e7eed586add54cafa0411c22
lim <sherwinlim8899@gmail.com>
2022-05-25 21:34:43 +0800

    7th commit

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
-import os
-if os.path.exists("python_files.txt"):
-    os.remove("python_files.txt")
+x = 500
+if x > 100:
+    raise Exception("if x is bigger than 100 this will be an error")
+
+try:
+    print(variable_1)
+
+except:
+    print("variable_1 is not defined")
+
+for i in range(6):
+    print("i'm so happy!")
+
+try:
+    print(12*6)
+except:
+    print("this operation cant be performed.")
 else:
-    print("File not exist")
+    print("this can be performed")
+
+best_burger = "Burger King"
+number_2_burger = "McDonald's"
+
+assert best_burger == "Burger King"
+assert best_burger == "McDonald's"
+
