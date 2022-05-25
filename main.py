bedd8a6d21ac40b07c3862bca51b8ce4b3bbe158
lim <sherwinlim8899@gmail.com>
2022-05-25 18:42:36 +0800

    3rd commit

--- main.py
+++ main.py
@@ -1,43 +1,11 @@
-class Clubmembers:
-    def __init__(self, name, birthday, age, favoritefood, goal):
-        self.name = name
-        self.birthday = birthday
-        self.age = age
-        self.favoritefood = favoritefood
-        self.goal = goal
+flavors = ["vanilla", "chocolate", "strawberry", "cookies_n_cream", "bubblegum"]
+toppings = ["almonds", "banana_slices", "chocolate syrup", "caramel syrup", "white_chocolate_chips"]
 
-    def dp1(self):
-        print("Name:", self.name)
-        print("Birthday:", self.birthday)
-        print("Age:", self.age)
-        print("Favorite Food:", self.favoritefood)
-        print("Goal:", self.goal)
+ice_cream = dict(zip(flavors, toppings))
 
+print(ice_cream)
 
-class Clubofficers(Clubmembers):
+ice_cream["chocolate"] = "blueberries"
+ice_cream.update({"matcha": "pistachios", "ube": "mango slices"})
 
-    def __init__(self, name, birthday, age, favoritefood, goal, position):
-        self.position = position
-        Clubmembers.__init__(self, name, birthday, age, favoritefood, goal)
-
-    def dp2(self):
-        print("Name:", self.name)
-        print("Birthday:", self.birthday)
-        print("Age:", self.age)
-        print("Favorite Food:", self.favoritefood)
-        print("Goal:", self.goal)
-        print("Position:", self.position)
-
-
-m_1 = Clubmembers("Tom", "January 16", 14, "Ice Cream", "To be happy")
-o_4 = Clubofficers("Vera", "June 22", 16, "Beef stroganoff", "To be the world's greatest chef", "Treasurer")
-
-m_1.dp1()
-o_4.dp2()
-
-
-
-
-
-
-
+print(ice_cream)
